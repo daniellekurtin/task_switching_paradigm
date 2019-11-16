1825d980d322041fd190c001aa64fb8037e8e876
mjaquiery <matt.jaquiery@psy.ox.ac.uk>
2019-11-16 13:04:30 +0000

    Spatial span
    Spatial span task.
    Some minor tidying of code.

--- src/taskSwitching/trial_spatial_span.py
+++ src/taskSwitching/trial_spatial_span.py
@@ -1,30 +1,136 @@
 from src.taskSwitching.trial import *
 
 
+def get_spatial_span_stimuli(n, n_rows=4, n_cols=4, span=4, numeral=None):
+    """
+    Generate digit span stimuli
+    :param n: number of stimuli to produce
+    :type n: int
+    :param n_cols: max column number for random calculation
+    :type n_cols: int
+    :param n_rows: max row number for random calculation
+    :type n_rows: int
+    :param span: number of digits in each stimulus
+    :type span: int
+    :param numeral: number to display in the various positions
+    :type numeral: int
+    :return: n stimulus nparrays
+    """
+    if span > n_rows * n_cols:
+        raise ValueError('Not possible to produce spatial span greater than number of cells')
+
+    stimuli = []
+    while len(stimuli) < n:
+        rows = []
+        cols = []
+        hashes = []
+        while len(rows) < span:
+            row = randint(0, n_rows - 1)
+            col = randint(0, n_cols - 1)
+
+            # We can hash row and column together to quickly check for clashes
+            h = hash((row, col))
+
+            if h not in hashes:
+                rows.append(row)
+                cols.append(col)
+                hashes.append(h)
+
+        if numeral is None:
+            v = randint(0, 9)
+        else:
+            v = numeral
+
+        stimulus = {
+            "rows": rows,
+            "cols": cols,
+            "numerals": [v] * len(rows)
+        }
+
+        if stimulus not in stimuli:
+            stimuli.append(stimulus)
+
+    output = []
+    for s in stimuli:
+        output.append([
+            make_display_numbers(
+                rows=[s["rows"][i]],
+                cols=[s["cols"][i]],
+                values=[s["numerals"][i]],
+                row_num=n_rows,
+                col_num=n_cols
+            ) for i in range(len(s["rows"]))
+        ])
+
+    return output
+
+
 class TrialSpatialSpan(Trial):
     def __init__(self, **kwargs):
-        super().__init__(kwargs)
+        super().__init__(**kwargs)
 
     def prepare_answers(self, override_existing=False):
-        if hasattr(self, "answers") and not override_existing:
-            return self.answers
+        if not (None in self.answers) and not override_existing:
+            return
 
-        n_rows = self.stimulus.size[0]
-        n_cols = self.stimulus.size[1]
-        values = self.stimulus
-        np.reshape(values, (1, np.prod(values.size)))
-        values = values[0]
-        indices = np.where(values is not None)
+        # Find the cell which has the value in it at each time point
+        rows = []
+        cols = []
+        numerals = []
+        for s in self.stimulus:
+            dim = np.shape(s)
+            n_rows = dim[0]
+            n_cols = dim[1]
+            values = s
+            values = np.reshape(values, (1, np.prod(np.shape(values))))
+            values = values[0]
+            indices = np.where([values[i] is not None for i in range(int(np.prod(np.shape(values))))])
+            indices = int(indices[0])  # only one answer!
+
+            values = s
+            values = np.reshape(values, (1, np.prod(np.shape(s))))
+
+            rows.append(floor(indices / n_cols))
+            cols.append(indices % n_cols)
+            numerals.append(values[0][indices])
 
         answer = make_display_numbers(
-            rows=[floor(i / n_cols) for i in indices],
-            cols=[i % n_cols for i in indices],
-            values=values,
+            rows=rows,
+            cols=cols,
+            values=numerals,
             row_num=n_rows,
             col_num=n_cols
         )
-        # foils = [make_display_numbers() for i in range(2)]
-        # foils.append(answer)
-        # shuffle(foils)
-        #
-        self.answers = answer
+
+        options = [answer]
+
+        while len(options) < len(self.answers):
+            rows = []
+            cols = []
+            hashes = []
+            while len(rows) < n_rows:
+                r = randint(0, n_rows - 1)
+                c = randint(0, n_cols - 1)
+                h = hash((r, c))
+                if h not in hashes:
+                    hashes.append(h)
+                    rows.append(r)
+                    cols.append(c)
+
+            foil = make_display_numbers(
+                rows=rows,
+                cols=cols,
+                values=numerals,
+                row_num=n_rows,
+                col_num=n_cols
+            )
+
+            if not any([np.array_equal(foil, o) for o in options]):
+                options.append(foil)
+
+        shuffle(options)
+        self.answerIndex = np.where([np.array_equal(answer, o) for o in options])
+        self.answerIndex = self.answerIndex[0][0]
+        self.answers = options
+
+        self.log('Target answer = ' + str(self.answerIndex))
