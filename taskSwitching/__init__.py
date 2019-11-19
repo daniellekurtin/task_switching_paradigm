5b6cc4631cabe28dac429a82b8b439ca905ec371
Hao-Ting Wang <htwangtw@gmail.com>
2019-11-19 20:08:39 +0000

    restructure the dir

---
+++ taskSwitching/__init__.py
@@ -0,0 +1,9 @@
+# expose package contents directly
+from taskSwitching.component import *
+from taskSwitching.experiment import *
+from taskSwitching.grid import *
+from taskSwitching.trial import *
+from taskSwitching.trial_digit_span import *
+from taskSwitching.trial_spatial_rotation import *
+from taskSwitching.trial_spatial_span import *
+from taskSwitching.component_rest import *
