Rotation fix
Fixed the rotation of stimuli for calculating true and foil answers for the spatial rotation task.

debug_main.py
@@ -30,8 +30,16 @@ __________________________________________
 """
 import taskSwitching as tS
 from psychopy import visual
-from random import shuffle
+from pyniexp import scannersynch
 import enum
+
+
+# Set some useful constants
+class Config(enum.Enum):
+    SYNCH_CONFIG = 'config.json'
+    IN_SCANNER = False
+    TR = 2 # seconds
+    MIN_LOG_LEVEL = 'INFO'
 
 
 class TrialTypes(enum.Enum):
@@ -45,76 +53,103 @@ class InfoCardDurations(enum.Enum):
     LONG = 4
 
 
-win = visual.Window(
-    size=[800, 800],
-    units="pix",
-    fullscr=False,
-    color=[0, 0, 0],
-    gammaErrorPolicy="warn"
-)
+class RunLength(enum.IntEnum):
+    MIN = 1
+    MAX = 3
 
-exp = tS.Experiment(window=win)
 
-n = 5
+class Block(enum.IntEnum):
+    COUNT = 1
+    TRIAL_COUNT = 9    # if this is not divisible by the number of task types things will go wrong
+    BREAK_TIME = 5
 
-stimuli = {
-    "SpatialSpan": tS.get_spatial_span_stimuli(n),
-    "DigitSpan": tS.get_digit_span_stimuli(n),
-    "SpatialRotation": tS.get_spatial_rotation_stimuli(n)
-}
 
-# Define experiment
-ic = [
-    tS.ComponentInfoCard(
-        experiment=exp,
-        next_task=TrialTypes.SPATIAL_ROTATION.value,
-        break_duration=InfoCardDurations.LONG.value
+if __name__ == '__main__':
+    # Create the window we'll display the experiment in
+    win = visual.Window(
+        size=[800, 800],
+        units="pix",
+        fullscr=False,
+        color=[0, 0, 0],
+        gammaErrorPolicy="warn"
     )
-]
 
-ss = [
-    tS.TrialSpatialSpan(
-        trialNumber=i,
-        experiment=exp,
-        stimulus=stimuli["SpatialSpan"][i]
-    ) for i in range(len(stimuli["SpatialSpan"]))
-]
+    # Create interface for scanner pulse and response box
+    SSO = scannersynch.scanner_synch(
+        config=Config.SYNCH_CONFIG.value,
+        emul_synch=not Config.IN_SCANNER.value,
+        emul_buttons=not Config.IN_SCANNER.value
+    )
+    SSO.set_synch_readout_time(0.5)
+    SSO.TR = Config.TR.value
 
-ds = [
-    tS.TrialDigitSpan(
-        trialNumber=i,
-        experiment=exp,
-        stimulus=stimuli["DigitSpan"][i]
-    ) for i in range(len(stimuli["DigitSpan"]))
-]
+    SSO.set_buttonbox_readout_time(0.5)
+    if not SSO.emul_buttons:
+        SSO.add_buttonbox('Nata')
+    else:
+        SSO.buttons = ['1','2','3']
 
-sr = [
-    tS.TrialSpatialRotation(
-        trialNumber=i,
-        experiment=exp,
-        stimulus=stimuli["SpatialRotation"][i]
-    ) for i in range(len(stimuli["SpatialRotation"]))
-]
+    SSO.start_process()
 
-# trials = ss + ds
-trials = sr
-shuffle(trials)
-trials = ic + trials
-trials.insert(n, tS.ComponentRest(break_duration=5, experiment=exp))
+    # Create the experiment object
+    exp = tS.Experiment(
+        window=win,
+        synch=SSO,
+        log_level=Config.MIN_LOG_LEVEL.value
+    )
 
-exp.trials = trials
-# exp.trials = [
-#     tS.Trial(
-#         trial_number = 0,
-#         experiment=exp,
-#         stimulus=[tS.make_display_numbers(
-#             rows=tS.np.repeat(list(range(4)), 4),
-#             cols=tS.np.tile(list(range(4)), 4),
-#             values=list(range(16)),
-#             row_num=4, col_num=4
-#         )],
-#         stimulus_duration=5
-#     )
-# ]
+    # Create stimuli. Expect this whole process will eventually be wrapped into the Experiment class
+    n = Block.TRIAL_COUNT * Block.COUNT
 
-exp.run()
+    stimuli = {
+        "SpatialRotation": tS.get_spatial_rotation_stimuli(n)
+    }
+
+    # Define experimental trials using stimuli
+    sr = [
+        tS.TrialSpatialRotation(
+            experiment=exp,
+            stimulus=stimuli["SpatialRotation"][i]
+        ) for i in range(len(stimuli["SpatialRotation"]))
+    ]
+
+    # Currently these need to be in the same order as the magazines defined below.
+
+    trials = []
+    # Construct experimental trial order
+    for b in range(Block.COUNT):          # Blocks
+        # Stack up the trials into a queue by type
+        magazines = [[], [], []]
+        for i in range(int(Block.TRIAL_COUNT)):
+            magazines[2].insert(i, sr.pop())
+
+        t = 0
+        magazine = 2
+        while t < Block.TRIAL_COUNT:     # Trials
+
+            # Create a new run of trials of a random length
+            # Guard against too large a minimum run length (orphan trials)
+            run_length = RunLength.MAX
+
+            # Create run
+
+            # Pop trials from magazine into the block definition
+            for r in range(run_length):     # Runs
+                trials.append(magazines[magazine].pop())
+                trials.append(tS.ComponentTrialGap(experiment=exp))
+
+            t += run_length
+
+        # Add a block break
+        if b < Block.COUNT - 1:
+            trials.append(tS.ComponentRest(experiment=exp, break_duration=Block.BREAK_TIME))
+
+    # Load trials into the experiment
+    exp.trials = trials
+
+    for t in exp.trials:
+        t.prepare()
+
+    # Run the experiment
+    exp.synch.wait_for_synch()
+    exp.run()

taskSwitching/trial_spatial_rotation.py
@@ -21,6 +21,9 @@ def get_spatial_rotation_stimuli(n, n_rows=4, n_cols=4, size=4, numeral=None):
     if size > n_rows * n_cols:
         raise ValueError('Not possible to produce spatial rotation shape larger than number of cells')
 
+    if n_rows != n_cols:
+        raise ValueError('Rotation only supports square grids (' + n_rows + 'x' + n_cols + ' given)')
+
     stimuli = []
     while len(stimuli) < n:
         rows = []
@@ -64,7 +67,7 @@ def get_spatial_rotation_stimuli(n, n_rows=4, n_cols=4, size=4, numeral=None):
                 stimulus["cols"][len(stimulus["cols"]) - 1]
             ])
             for deg in [90, 180, 270]:
-                if np.all((np.equal(check, translate_and_rotate(check, deg)))):
+                if np.all((np.equal(check, rotate(check, n_rows, deg)))):
                     append = False
                     break
 
@@ -86,49 +89,34 @@ def get_spatial_rotation_stimuli(n, n_rows=4, n_cols=4, size=4, numeral=None):
     return output
 
 
-def translate_and_rotate(matrix, rotate_deg=90, translation_vector=None):
+def rotate(matrix, grid_size, deg=90):
     """
-    :param matrix: matrix to manipulate
+    Rotate an n x n grid clockwise.
+    For a 90 degree clockwise rotation each cell (r, c) gets new coordinates (c, (n-1) - r).
+    :param matrix: matrix of coordinates to manipulate
     :type matrix: nparray
-    :param rotate_deg: number of degrees to rotate anticlockwise
-    :type rotate_deg: int
-    :param translation_vector: 3x3 matrix containing the translation to prepend to the rotation and to invert
-    afterwards, default (-2, -2)
-    :type translation_vector: nparray|None
-    :return: new matrix resulting from translating, rotating, and then anti-translating the original matrix
+    :param grid_size: number of cells in the grid
+    :param deg: degrees to rotate, only steps of 90 will work
+    :type deg: int
+    :return: new matrix with rotated coordinates
     """
-    radians = math.radians(rotate_deg)
 
-    if translation_vector is None:
-        translation_vector = np.array([
-            [-1.5],  # (n_row - 1) / 2
-            [-1.5],  # (n_col - 1) / 2
-            [0]
-        ])
+    if deg % 90 != 0:
+        raise ValueError("rotate() only supports rotations of 90 degrees")
 
-    rotation_matrix = np.array([
-        [math.cos(radians), -math.sin(radians), 0],
-        [math.sin(radians), math.cos(radians), 0],
-        [0, 0, 1]
-    ])
+    # Recursively call rotate() to rotate by 90deg until we're 90deg away from target rotation
+    # It would technically be more efficient to do this as a yielding iterative, but we'll stick with this for now
+    if deg > 90:
+        matrix = rotate(matrix, grid_size, deg - 90)
 
-    if np.size(matrix, 0) == 2:
-        matrix = np.vstack((
-            matrix,
-            np.repeat(1, np.size(matrix, 1))
-        ))
-        trim = True
-    else:
-        trim = False
+    new_matrix = np.zeros_like(matrix)
 
-    new_matrix = np.add(translation_vector, matrix)
-    new_matrix = np.dot(rotation_matrix, new_matrix)
-    new_matrix = np.add(translation_vector * -1, new_matrix)  # translate back
+    # New row <- old column
+    new_matrix[0] = matrix[1]
+    # New column <- (n - 1) - old row
+    new_matrix[1] = (grid_size - 1) - matrix[0]
 
     # reshape the matrix back to the form of the input
-    if trim:
-        new_matrix = new_matrix[[0, 1], :]
-
     return new_matrix.astype(int)
 
 
@@ -158,8 +146,9 @@ class TrialSpatialRotation(Trial):
                 rows.append(floor(i / n_cols))
                 cols.append(i % n_cols)
 
-        rotated = translate_and_rotate(
-            np.array([rows, cols])
+        rotated = rotate(
+            np.array([rows, cols]),
+            n_rows
         )
 
         answer = make_display_numbers(
@@ -191,7 +180,7 @@ class TrialSpatialRotation(Trial):
             okay = True
             check = np.array([rows, cols])
             for deg in [90, 180]:  # 270 covered already by the correct answer
-                if np.all(np.equal(check, translate_and_rotate(check, deg))):
+                if np.all(np.equal(check, rotate(check, n_rows, deg))):
                     okay = False
                     break
 