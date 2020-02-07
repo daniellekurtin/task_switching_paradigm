f8720592723665047b88e885b25df93c437770ce
mjaquiery <matt.jaquiery@psy.ox.ac.uk>
2020-02-07 14:13:03 +0000

    tidying analysis script
    Made file path relative.
    Removed some lint.

--- behav_analysis.py
+++ behav_analysis.py
@@ -3,39 +3,59 @@ import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.backends.backend_pdf import PdfPages
 
-###
-###
-###
-###
-###
 # columns of interest from .csv file
-headers = ['participant_id', 'type', 'target_ans', 'participant_ans', 'time_start', 'time_response_enabled', 'time_response_submitted']
+headers = [
+    'participant_id',
+    'type',
+    'target_ans',
+    'participant_ans',
+    'time_start',
+    'time_response_enabled',
+    'time_response_submitted'
+]
 # reading in .csv file
-df = pd.read_csv(r'C:\Users\danie\Documents\SURREY\Project_1\oxbridge_brainhack_2019\data\public\ExperimentTaskSwitch-v0.0.2_trials-v0.0.1.csv', usecols = headers)
-###
-###
-###
-###
-###
+df = pd.read_csv(r'data/public/ExperimentTaskSwitch-v0.0.2_trials-v0.0.1.csv', usecols=headers)
 
 # creating an empty df with column names only for data analysis later on
-df_new = pd.DataFrame(columns=['participant_id', 'type', 'target_ans', 'participant_ans', 'time_start', 'time_response_enabled', 'time_response_submitted', 'time_block', 'response_time', 'accuracy', 'block'])
-df_accuracy = pd.DataFrame(columns=['participant_id', 'type', 'block', 'accuracy', 'cumu_acc'])
+df_new = pd.DataFrame(columns=[
+    'participant_id',
+    'type',
+    'target_ans',
+    'participant_ans',
+    'time_start',
+    'time_response_enabled',
+    'time_response_submitted',
+    'time_block',
+    'response_time',
+    'accuracy',
+    'block'
+])
+df_accuracy = pd.DataFrame(columns=[
+    'participant_id',
+    'type',
+    'block',
+    'accuracy',
+    'cumu_acc'
+])
 
-#-----DATA-----
-#-calculating blocks-
+# -----DATA-----
+# calculating blocks
 
 # creating new df columns
 df['time_block'] = df.time_start.diff()
 
 # defining RT and A
-df['response_time'] = np.where(df['time_response_submitted'] == int(-1), 3, df['time_response_submitted'].sub(df['time_response_enabled'], axis = 0)) 
+df['response_time'] = np.where(
+    df['time_response_submitted'] == int(-1),
+    3,
+    df['time_response_submitted'].sub(df['time_response_enabled'], axis=0)
+)
 
 conditions = [
     df['target_ans'] == df['participant_ans']
     ]
 choices = [int(1)]
-df['accuracy'] = np.select(conditions, choices, default = 0)
+df['accuracy'] = np.select(conditions, choices, default=0)
 
 # grouping by participant_id
 pid_group = df.groupby('participant_id')
@@ -51,8 +71,8 @@ for group_name, df_group in pid_group:
             x = x
         df_group.at[row_index,'block'] = x
     df_new = pd.concat([df_new, df_group]) 
-    df_new.reset_index(drop = True, inplace = True)
-print('\n********************************************************************************************************************************************')
+    df_new.reset_index(drop=True, inplace=True)
+print('\n************************************************************************************************')
 
 # # *****troubleshoot
 # print(df_new)
@@ -61,16 +81,22 @@ print('\n***********************************************************************
 # exporting data to .csv file 
 df_new.to_csv(r'data1')
 
+# -----ANALYSIS-----
 
-
-#-----ANALYSIS-----
-
-# heirachical grouping of data
+# heirarchical grouping of data
 df_new.set_index(['participant_id', 'type', 'block'], inplace = True)
 types = ['TrialDigitSpan', 'TrialSpatialSpan', 'TrialSpatialRotation']
 
-#---MRT---
-df_mrt = df_new.drop(columns = ['time_start', 'time_response_enabled', 'time_response_submitted', 'time_block', 'target_ans', 'participant_ans', 'accuracy'])
+# ---MRT---
+df_mrt = df_new.drop(columns = [
+    'time_start',
+    'time_response_enabled',
+    'time_response_submitted',
+    'time_block',
+    'target_ans',
+    'participant_ans',
+    'accuracy'
+])
 
 # mean response time (mrt) calc. for each block, per type, per participant
 for title, df_mrt1 in df_mrt.groupby(level=[0, 1]):
@@ -82,11 +108,19 @@ for title, df_mrt1 in df_mrt.groupby(level=[0, 1]):
     #*****
     print('\nPLOTTING MRT (participant_id, type) = {}('.format(title))
     mrt.plot(legend = True).get_figure().savefig('MRT/MRT: {}'.format(title))
-print('\n********************************************************************************************************************************************')
+print('\n************************************************************************************************')
 
-#---ACCURACY---
+# ---ACCURACY---
 
-df_acc = df_new.drop(columns = ['time_start', 'time_response_enabled', 'time_response_submitted', 'time_block', 'target_ans', 'participant_ans', 'response_time'])
+df_acc = df_new.drop(columns = [
+    'time_start',
+    'time_response_enabled',
+    'time_response_submitted',
+    'time_block',
+    'target_ans',
+    'participant_ans',
+    'response_time'
+])
 
 # calc. cumulative accuracy for each block, per type, per participant
 for title, df_acc1 in df_acc.groupby(level=[0, 1, 2]): 
@@ -104,7 +138,7 @@ for title, df_acc1 in df_acc.groupby(level=[0, 1, 2]):
     #print(df_acc1)
     #*****
     print('\nCUMULATIVE ACCURACY FOR (participant_id, type, block) = {}('.format(title))
-print('\n********************************************************************************************************************************************')
+print('\n************************************************************************************************')
 
 df_accuracy.set_index(['participant_id', 'type', 'block'], inplace = True)
 df_accuracy1 = df_accuracy.drop(columns = ['accuracy'])
