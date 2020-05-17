f9b802059d93d08781ec33deb63d2be168843add
daniellekurtin <danielle.kurtin1@gmail.com>
2020-05-17 15:48:40 +0100

    adding consent tickbox

--- www/taskSwitchingPlugins/ts-ethics.js
+++ www/taskSwitchingPlugins/ts-ethics.js
@@ -9,10 +9,14 @@ var ethics = {
         
         'The whole experiment will take around 20 minutes. Before starting the experiment, you will be asked a few demographic questions.',
         
-        'Before starting the experiment, please make sure: You have understood the basic information about this experiment. You are aware of what your participation involves. You confirm you do not have a history of migraine or epilepsy or any other neurological or psychiatric conditions. You understand that by taking part you are consenting to your data being used to assist in research into neuroscience and cognition. The information we collect will be analysed anonymously and we do not store any personally identifiable information (such as IP address location name or contact details). Anonymised data collected up to the point of withdrawal will be kept (data from tests and answers to the demographic questions). Your participation in this research is completely VOLUNTARY. If you choose to participate you may subsequently withdraw from the study at any time without penalty or consequences of any kind. Please read carefully and make sure you have understood the information above.',
+        'Before starting the experiment, please make sure: You have understood the basic information about this experiment. You are aware of what your participation involves. You confirm you do not have a history of migraine or epilepsy or any other neurological or psychiatric conditions. You understand that by taking part you are consenting to your data being used to assist in research into neuroscience and cognition. The information we collect will be analysed anonymously and we do not store any personally identifiable information (such as IP address location name or contact details). Anonymised data collected up to the point of withdrawal will be kept (data from tests and answers to the demographic questions). Your participation in this research is completely VOLUNTARY. If you choose to participate you may subsequently withdraw from the study at any time without penalty or consequences of any kind. Please read carefully and make sure you have understood the information above.'
         
-        'If you agree, click "I agree" to continue'
-
     ],
     show_clickable_nav: true
 }
+
+var consent = {
+    type:'consent',
+    checkbox_text: 'I agree to participate in this study.',
+    button_text: "Begin experiment!"
+  };
